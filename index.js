bb731016f5dbc8025fe4c3a69893bc0f7002ab26
Rajja Farhan <rajjafarhan2@gmail.com>
2023-08-23 03:30:55 +0500

    CLI node js

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import './src/command.js'
+
+//we only place this line on the top of our entry file not in every file,
+
+// import fs from 'node:fs'
+// import http from 'node:http' //a module which help us in networking
+
+// const note= process.argv[2] //bcs on 1st index it will be path to node execetuable and on 2nd index it will path to this file and argv is used to generate an array
+// const newNote={
+//     content:note,
+//     id:Date.now()
+// }
+// console.log(newNote);
+
+// import {readFile} from 'fs/promises';
+// // console.log(import.meta.url)
+//  let template= await readFile(new URL("template.html",import.meta.url)) 
+
